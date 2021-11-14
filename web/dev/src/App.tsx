b5db8d9f0aac846df98c508f37b894f840ec29e7
dima <d.ovdenko@bi.zone>
2021-11-14 22:00:29 +0300

    proxy removed + registration

--- web/dev/src/App.tsx
+++ web/dev/src/App.tsx
@@ -3,20 +3,11 @@ import Form from "./components/Form";
 import Modal from "./components/Modal";
 import {useTypedSelector} from "./hooks/useTypedSelector";
 import {Route, Switch, useHistory} from "react-router-dom";
-import {useEffect} from "react";
-import DataService from "./data/DataService";
 
 function App() {
     const auth = useTypedSelector((state) => state.auth);
     const modal = useTypedSelector((state) => state.modal);
     const history = useHistory();
-    console.log("user", auth);
-
-    useEffect(() => {
-        DataService.curUser()
-            .then((res) => console.log(res))
-            .catch((error) => console.error('er', error))
-    }, [])
 
     return (
         <div className="App">
