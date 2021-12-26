b5af9cc50a715112519beb651e494d795e3ad6a1
Elderly-AI <aae_pl@mail.ru>
2021-12-26 14:54:00 +0300

    [TAB-14] add calculation in additional code with corrective step

--- app/internal/pkg/model/calculations.go
+++ app/internal/pkg/model/calculations.go
@@ -1,5 +1,26 @@
 package model
 
+import (
+	"fmt"
+	"strconv"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/Elderly-AI/ta_eos/internal/pkg/value_lib"
+)
+
+func CreateValueFromString(val string, grid uint8, valueType value_lib.ValueType) (value_lib.Value, error) {
+	value, err := strconv.ParseUint(val, 2, 64)
+	if err != nil {
+		return value_lib.Value{}, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Value %s have invalid format", val),
+		)
+	}
+	return value_lib.InitValue(value, grid, valueType), nil
+}
+
 type Number struct {
 	Value    uint64
 	BinValue string
@@ -8,6 +29,12 @@ type Number struct {
 type CalculationRequest struct {
 	Multiplier *Number
 	Factor     *Number
+	GridSize   uint32
+}
+
+type CalculationRequestV2 struct {
+	Multiplier value_lib.Value
+	Factor     value_lib.Value
 	GridSize   uint32
 }
 
--- app/pkg/proto/calculations/calculations.pb.gw.go
+++ app/pkg/proto/calculations/calculations.pb.gw.go
@@ -167,6 +167,40 @@ func local_request_Calculations_DirectCodeHighDigitsRightShiftCalculation_0(ctx 
 
 }
 
+func request_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(ctx context.Context, marshaler runtime.Marshaler, client CalculationsClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AdditionalCodeWithCorrectiveStepRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.AdditionalCodeWithCorrectiveStepCalculation(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(ctx context.Context, marshaler runtime.Marshaler, server CalculationsServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AdditionalCodeWithCorrectiveStepRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.AdditionalCodeWithCorrectiveStepCalculation(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterCalculationsHandlerServer registers the http handlers for service Calculations to "mux".
 // UnaryRPC     :call CalculationsServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -262,6 +296,29 @@ func RegisterCalculationsHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		}
 
 		forward_Calculations_DirectCodeHighDigitsRightShiftCalculation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/calculations.Calculations/AdditionalCodeWithCorrectiveStepCalculation", runtime.WithHTTPPathPattern("/api/v1/calculations/additional_code/corrective_step"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -386,6 +443,26 @@ func RegisterCalculationsHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
+	mux.Handle("POST", pattern_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/calculations.Calculations/AdditionalCodeWithCorrectiveStepCalculation", runtime.WithHTTPPathPattern("/api/v1/calculations/additional_code/corrective_step"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -397,6 +474,8 @@ var (
 	pattern_Calculations_DirectCodeLowDigitsRightShiftCalculation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 2, 5}, []string{"api", "v1", "calculations", "direct_code", "low_digits", "right_shift"}, ""))
 
 	pattern_Calculations_DirectCodeHighDigitsRightShiftCalculation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 2, 5}, []string{"api", "v1", "calculations", "direct_code", "high_digits", "right_shift"}, ""))
+
+	pattern_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "v1", "calculations", "additional_code", "corrective_step"}, ""))
 )
 
 var (
@@ -407,4 +486,6 @@ var (
 	forward_Calculations_DirectCodeLowDigitsRightShiftCalculation_0 = runtime.ForwardResponseMessage
 
 	forward_Calculations_DirectCodeHighDigitsRightShiftCalculation_0 = runtime.ForwardResponseMessage
+
+	forward_Calculations_AdditionalCodeWithCorrectiveStepCalculation_0 = runtime.ForwardResponseMessage
 )
