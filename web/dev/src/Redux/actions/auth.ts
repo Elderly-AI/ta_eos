bb59dc65e8cde6538d400b91276c1dab6caea974
sn.alekseev <sn.alekseev@corp.vk.com>
2021-12-26 15:49:32 +0300

    TAF-17: Add binding header with role user

--- web/dev/src/Redux/actions/auth.ts
+++ web/dev/src/Redux/actions/auth.ts
@@ -38,20 +38,3 @@ export const getCurrentUser = () => {
             });
     };
 };
-
-// export const authInside = () => {
-//     console.log('authInside >>> ')
-//     return (dispatch: Dispatch<authAction>) => {
-//         fetch(api.inside).then((res) => {
-//             if (res.status !== 400) {
-//                 res.json().then((json) => {
-//                     json.token = document.cookie.split("=")[1];
-//                     dispatch({type: authAPI.INSIDE, payload: json as IAuth, inside: true})
-//                 })
-//                 // dispatch({ type: authAPI.INSIDE, payload: res.json() as IAuth, inside: true });
-//             } else {
-//                 dispatch({type: authAPI.NOT_INSIDE});
-//             }
-//         });
-//     };
-// };
--- web/dev/src/components/Header/Header.tsx
+++ web/dev/src/components/Header/Header.tsx
@@ -1,14 +1,75 @@
-import {AppBar, Toolbar} from '@material-ui/core';
-import React from 'react';
+import {AppBar, Toolbar, makeStyles, Button} from '@material-ui/core';
+import {blue} from '@material-ui/core/colors';
+import {Link as RouterLink} from 'react-router-dom';
+import React, {useState} from 'react';
+import {useTypedSelector} from '@hooks/useTypedSelector';
+
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: blue[900],
+        paddingRight: '79px',
+    },
+    title: {
+        fontWeight: 600,
+        color: 'white',
+        textAlign: 'left',
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    adminButton: {
+        fontWeight: 700,
+        size: '18px',
+    },
+}));
 
 export default function Header() {
-    const displayDesktop = () => {
-        return <Toolbar>Тренажер по теории автоматов</Toolbar>;
+    const {header, title, adminButton, toolbar} = useStyles();
+
+    const user = useTypedSelector((state) => state.auth);
+
+    const contentHeaderComponent = () => {
+        return (
+            <Toolbar className={toolbar}>
+                {titleComponent}
+                <div>{adminButtonComponent()}</div>
+            </Toolbar>
+        );
+    };
+
+    const titleComponent = (
+        <Button variant="text" className={title}
+            {...{
+                color: 'inherit',
+                to: '/home',
+                component: RouterLink,
+            }}
+        >
+        Тренажер по ТА
+        </Button>
+    );
+
+    const adminButtonComponent = () => {
+        if (user?.role !== 'admin') {
+            return null;
+        }
+        return (
+            <Button className={adminButton}
+                {...{
+                    color: 'inherit',
+                    to: '/admin',
+                    component: RouterLink,
+                }}
+            >
+            Управление
+            </Button>
+        );
     };
 
     return (
         <header>
-            <AppBar>{displayDesktop()}</AppBar>
+            <AppBar className={header}>{contentHeaderComponent()}</AppBar>
         </header>
     );
 }
