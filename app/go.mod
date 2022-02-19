5f0e1baf5b416d1c9144f7935fe2dbb049fec1c9
Elderly-AI <aae_pl@mail.ru>
2022-02-20 02:26:07 +0300

    [TAB-17] improve num lib

--- app/go.mod
+++ app/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.10.4
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+	github.com/stretchr/testify v1.7.0
 	google.golang.org/genproto v0.0.0-20211001223012-bfb93cce50d9
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
@@ -16,9 +17,12 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- app/internal/app/template/template.go
+++ app/internal/app/template/template.go
@@ -2,26 +2,33 @@ package template
 
 import (
 	"context"
-	templateRepo "github.com/Elderly-AI/ta_eos/internal/pkg/database/template"
-	pb "github.com/Elderly-AI/ta_eos/pkg/proto/template"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	templateRepo "github.com/Elderly-AI/ta_eos/internal/pkg/database/template"
+	templateFacade "github.com/Elderly-AI/ta_eos/internal/pkg/template"
+	pb "github.com/Elderly-AI/ta_eos/pkg/proto/template"
 )
 
 type Server struct {
-	repo templateRepo.Repo
+	repo   *templateRepo.Repo
+	facade *templateFacade.Facade
 	pb.UnimplementedTemplateServer
 }
 
 func (s Server) GetKrHandler(ctx context.Context, request *pb.KrRequest) (*pb.OkMessage, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetKrHandler not implemented")
 }
+
 func (s Server) ApproveKrHandler(context.Context, *pb.KrRequest) (*pb.OkMessage, error) {
+
 	return nil, status.Errorf(codes.Unimplemented, "method ApproveKrHandler not implemented")
 }
 
-func NewTemplateHandler(repo templateRepo.Repo) Server {
+func NewTemplateHandler(repo *templateRepo.Repo, facade *templateFacade.Facade) Server {
 	return Server{
-		repo: repo,
+		repo:   repo,
+		facade: facade,
 	}
 }
