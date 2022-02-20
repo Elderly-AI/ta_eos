33a9850ac939bfe57983486ea804557fcfcf24f7
Дмитрий Гуляченков <d.gulyachenkov@corp.mail.ru>
2022-02-20 17:54:00 +0300

    [dima] update proto

--- app/internal/app/template/template.go
+++ app/internal/app/template/template.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	"context"
+	"fmt"
 	templateRepo "github.com/Elderly-AI/ta_eos/internal/pkg/database/template"
 	pb "github.com/Elderly-AI/ta_eos/pkg/proto/template"
 	"google.golang.org/grpc/codes"
@@ -14,7 +15,11 @@ type Server struct {
 }
 
 func (s Server) GetKrHandler(ctx context.Context, request *pb.KrRequest) (*pb.OkMessage, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetKrHandler not implemented")
+	fmt.Println(request.Data)
+	return &pb.OkMessage{
+		Ok:    true,
+		Error: "",
+	}, nil
 }
 func (s Server) ApproveKrHandler(context.Context, *pb.KrRequest) (*pb.OkMessage, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ApproveKrHandler not implemented")
