7970845193b4dc3f37c512cc3dff38fad3cba19f
Alkirys <nik_manz@mail.ru>
2022-03-02 22:05:27 +0300

    TAF-42: верстка таблицы

--- web/dev/src/components/Work/CustomTable.tsx
+++ web/dev/src/components/Work/CustomTable.tsx
@@ -1,79 +1,93 @@
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField} from '@material-ui/core';
-import React, {useState} from 'react';
+import React, {Dispatch, SetStateAction, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+import {TableState} from './Work';
 
 const useStyles = makeStyles({
-    tableView: {
-        // backgroundColor: 'red',
+    tableRow: {
+        height: '70px',
     },
 
     input: {
-        // width: 'min-content',
         width: '100px',
     },
 });
 
-const CustomTable = () => {
+interface CustomTableProps {
+    array: TableState[],
+    setArray: Dispatch<SetStateAction<TableState[]>>,
+}
+
+const CustomTable = ({array, setArray}: CustomTableProps) => {
     const styles = useStyles();
     const [inputNumber, setInputNumber] = useState(0);
-    const [arr, setArr] = useState<number[]>([1, 2, 3, 4]);
-    const [state, setState] = useState('10010');
+    const [state, setState] = useState('');
 
     const cellClickHandler = (evt: any) => {
-        console.log('click', evt.currentTarget.id.split('_'));
         const id = +evt.currentTarget.id.split('_')[1];
         if (id !== inputNumber) {
-            setState(arr[id].toString());
+            setState(array[id%3+1].data[~~(id/3)].value?.toString() || '');
             setInputNumber(id);
         }
     };
 
     return (
-        <TableContainer component={Paper} className={styles.tableView}>
+        <TableContainer component={Paper}>
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align="left"/>
-                        <TableCell align="left">Прямой</TableCell>
-                        <TableCell align="left">Обратный</TableCell>
-                        <TableCell align="left">Дополнительный</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
                         {
-                            [0, 1, 2, 3].map((cur) => (
-                                <TableCell
-                                    id={'cell_' + cur}
-                                    key={'cell_' + cur}
-                                    width={'25%'}
-                                    onClick={cellClickHandler}
-                                >
-                                    {
-                                        inputNumber === cur ?
-                                            <TextField
-                                                variant="standard"
-                                                className={styles.input}
-                                                value={state}
-                                                onChange={(evt) => {
-                                                    setArr((arr) => {
-                                                        arr[cur] = +evt.target.value;
-                                                        // arr.push(Number(evt.target.value));
-                                                        return arr;
-                                                    });
-                                                    setState(evt.target.value);
-                                                    console.log(evt.target.value);
-                                                    console.log(arr);
-                                                }
-                                                }
-                                                autoFocus
-                                            /> :
-                                            arr[cur]
-                                    }
+                            array.map((cur) => (
+                                <TableCell align="left" width={'25%'} key={'headCell_' + cur.name}>
+                                    {cur.name}
                                 </TableCell>
                             ))
                         }
                     </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        array[0].data.map((current, idx) => (
+                            <TableRow key={'row_' + idx} className={styles.tableRow}>
+                                {
+                                    array.map((cur, index) => (
+                                        index === 0 ?
+                                            <TableCell
+                                                key={'leftCell_' + idx * 3 + index}
+                                                width={'25%'}
+                                            >
+                                                {cur.data[idx].name}
+                                            </TableCell> :
+                                            <TableCell
+                                                id={'cell_' + (idx * 3 + index - 1)}
+                                                key={'cell_' + (idx * 3 + index - 1)}
+                                                width={'25%'}
+                                                onClick={cellClickHandler}
+                                            >
+                                                {
+                                                    inputNumber === idx * 3 + index - 1 ?
+                                                        <TextField
+                                                            variant="standard"
+                                                            className={styles.input}
+                                                            value={state}
+                                                            onChange={(evt) => {
+                                                                setArray((arr) => {
+                                                                    cur.data[idx].value = evt.target.value;
+                                                                    return arr;
+                                                                });
+                                                                setState(evt.target.value);
+                                                            }
+                                                            }
+                                                            autoFocus
+                                                        /> :
+                                                        cur.data[idx].value
+                                                }
+                                            </TableCell>
+                                    ))
+                                }
+                            </TableRow>
+                        ))
+                    }
                 </TableBody>
             </Table>
         </TableContainer>
