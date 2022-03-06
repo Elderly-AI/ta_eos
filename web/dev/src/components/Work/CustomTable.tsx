5588b58ee4cb406f152c901ab56ac84b907cf450
Alkirys <nik_manz@mail.ru>
2022-03-07 01:15:43 +0300

    TAF-43: рабочие copy/paste без доступа к clipboard

--- web/dev/src/components/Work/CustomTable.tsx
+++ web/dev/src/components/Work/CustomTable.tsx
@@ -12,7 +12,6 @@ import {
 import React, {Dispatch, SetStateAction, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {TableState} from './Work';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
 import classNames from 'classnames';
 import useClippy from 'use-clippy';
 
@@ -65,7 +64,8 @@ const useStyle = makeStyles({
 
 interface InputCellProps {
     inputValue: string,
-    onChange: (evt: any, value?: string) => void;
+    onChange: (evt: any, value?: string) => void,
+    copiedText: string,
 }
 
 interface CustomTableProps {
@@ -73,17 +73,14 @@ interface CustomTableProps {
     setArray: Dispatch<SetStateAction<TableState[]>>,
 }
 
-const TextCell = (props: {cellText: string | null}) => {
-    const {cellText} = props;
+const TextCell = (props: {cellText: string | null, copyText: (text: string) => void}) => {
+    const {cellText, copyText} = props;
     const styles = useStyles();
     const classes = useStyle();
 
     const clickHandler = (evt: React.MouseEvent) => {
         evt.stopPropagation();
-        navigator.clipboard.readText().then(
-            (data) => {
-                console.log('copied', data);
-            });
+        copyText(cellText || '');
     };
 
     if (!cellText) {
@@ -92,36 +89,21 @@ const TextCell = (props: {cellText: string | null}) => {
         return (
             <div className={styles.iconContainer}>
                 <Typography variant="subtitle1">{cellText}</Typography>
-                <CopyToClipboard text={cellText || ''}>
-                    <svg className={classNames(styles.icon, classes.root)} viewBox="0 0 24 24" onClick={clickHandler}>
-                        {/* eslint-disable-next-line */}
-                        <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
-                    </svg>
-                </CopyToClipboard>
+                <svg className={classNames(styles.icon, classes.root)} viewBox="0 0 24 24" onClick={clickHandler}>
+                    {/* eslint-disable-next-line */}
+                    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
+                </svg>
             </div>
         );
     }
 };
 
-const InputCell = ({inputValue, onChange}: InputCellProps) => {
+const InputCell = ({inputValue, onChange, copiedText}: InputCellProps) => {
     const styles = useStyles();
-    const [text, setText] = useClippy();
 
     const clickHandler = (evt: React.MouseEvent) => {
         evt.stopPropagation();
-        const a = `${Math.random()%100}`;
-        console.log('random', a);
-        console.log(text);
-        setText(a);
-        console.log(text);
-        onChange(undefined, inputValue + text);
-        // if (!navigator.clipboard) return;
-        // navigator.clipboard.readText().then(
-        //     (data) => {
-        //         // это я предпочел поизгаляться над ивентом, нежели прокидывать в компонент 100500 пропсов
-        //         onChange(undefined, inputValue + data);
-        //         console.log('paste', data);
-        //     });
+        onChange(undefined, inputValue + copiedText);
     };
 
     return (
@@ -131,7 +113,6 @@ const InputCell = ({inputValue, onChange}: InputCellProps) => {
                 className={styles.input}
                 value={inputValue}
                 onChange={onChange}
-                onBlur={(evt) => evt.currentTarget.focus()}
                 autoFocus
                 focused={true}
             />
@@ -147,6 +128,7 @@ const CustomTable = ({array, setArray}: CustomTableProps) => {
     const styles = useStyles();
     const [inputNumber, setInputNumber] = useState(0);
     const [inputText, setInputText] = useState('');
+    const [text, setText] = useClippy();
 
     const cellClickHandler = (evt: any) => {
         const id = +evt.currentTarget.id.split('_')[1];
@@ -161,71 +143,72 @@ const CustomTable = ({array, setArray}: CustomTableProps) => {
     }
 
     return (
-        <>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            {array.map((cur) => (
-                                <TableCell
-                                    className={styles.tableHead}
-                                    align="left"
-                                    width={'25%'}
-                                    key={'headCell_' + cur.name}
-                                >
-                                    {cur.name}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {array[0].data.map((current, idx) => (
-                            // тут ебнутая логика с массивом: данные лежат в нем по столбцам, а таблица
-                            // строится по строкам
-                            // первый map идет по 1(не важно какому) столбцу и задает только номер текущей строки(idx)
-                            <TableRow key={'row_' + idx} className={styles.tableRow} hover>
-                                {array.map((cur, index) => {
-                                    // второй map идет по самим столбцам(массиву) и забирает по одному значению из них
-                                    // согласно номеру строки. Искренне надеюсь, что это не придется переписывать)
-                                    const tmpCellNumber = idx * 3 + index - 1;
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        {array.map((cur) => (
+                            <TableCell
+                                className={styles.tableHead}
+                                align="left"
+                                width={'25%'}
+                                key={'headCell_' + cur.name}
+                            >
+                                {cur.name}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {array[0].data.map((current, idx) => (
+                        // тут ебнутая логика с массивом: данные лежат в нем по столбцам, а таблица строится по строкам
+                        // первый map идет по 1(не важно какому) столбцу и задает только номер текущей строки(idx)
+                        <TableRow key={'row_' + idx} className={styles.tableRow} hover>
+                            {array.map((cur, index) => {
+                                // второй map идет по самим столбцам(массиву) и забирает по одному значению из них
+                                // согласно номеру строки. Искренне надеюсь, что это не придется переписывать)
+                                const tmpCellNumber = idx * 3 + index - 1;
 
-                                    if (index === 0) {
-                                        return (
-                                            <TableCell key={'leftCell_' + tmpCellNumber + 1} width="25%">
-                                                {cur.data[idx].name}
-                                            </TableCell>
-                                        );
-                                    }
-
-                                    const changeHandler = (evt: any, value?: string) => {
-                                        setArray((arr) => {
-                                            arr[index].data[idx].value = evt?.target.value || value || '';
-                                            return arr;
-                                        });
-                                        setInputText(evt?.target.value || value || '');
-                                    };
-
+                                if (index === 0) {
                                     return (
-                                        <TableCell
-                                            id={'cell_' + tmpCellNumber}
-                                            key={'cell_' + tmpCellNumber}
-                                            width={'25%'}
-                                            onClick={cellClickHandler}
-                                            className={styles.pointer}
-                                        >
-                                            {inputNumber === tmpCellNumber ?
-                                                <InputCell inputValue={inputText} onChange={changeHandler}/> :
-                                                <TextCell cellText={cur.data[idx].value} />
-                                            }
+                                        <TableCell key={'leftCell_' + tmpCellNumber + 1} width="25%">
+                                            {cur.data[idx].name}
                                         </TableCell>
                                     );
-                                })}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
+                                }
+
+                                const changeHandler = (evt: any, value?: string) => {
+                                    setArray((arr) => {
+                                        arr[index].data[idx].value = evt?.target.value || value || '';
+                                        return arr;
+                                    });
+                                    setInputText(evt?.target.value || value || '');
+                                };
+
+                                return (
+                                    <TableCell
+                                        id={'cell_' + tmpCellNumber}
+                                        key={'cell_' + tmpCellNumber}
+                                        width={'25%'}
+                                        onClick={cellClickHandler}
+                                        className={styles.pointer}
+                                    >
+                                        {inputNumber === tmpCellNumber ?
+                                            <InputCell
+                                                inputValue={inputText}
+                                                onChange={changeHandler}
+                                                copiedText={text}
+                                            /> :
+                                            <TextCell cellText={cur.data[idx].value} copyText={setText}/>
+                                        }
+                                    </TableCell>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
