05ec1fec043d7578e84c00a2a5019d8540e5c254
Дмитрий Гуляченков <d.gulyachenkov@corp.mail.ru>
2022-03-10 10:06:31 +0300

    [dima] fix table

--- app/migrations/006_create_template_table.sql
+++ app/migrations/006_create_template_table.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS templates
 (
-    user_id    SERIAL PRIMARY KEY,
+    user_id    text,
     kr_name    VARCHAR(256) default '',
     template   jsonb        default null,
     saved_date timestamp
