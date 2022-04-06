9bada0fa8b333cf2242305c75c6b783f788e65fe
Alkirys <nik_manz@mail.ru>
2022-04-06 23:04:18 +0300

    TAB-__: fix docker

--- app/migrations/005_ocenki_field.sql
+++ app/migrations/005_ocenki_field.sql
@@ -3,6 +3,8 @@
 
 ALTER TABLE users
     ADD COLUMN IF NOT EXISTS grades jsonb not null default '{}'::jsonb;
+INSERT INTO users(id, name, email, study_group, password, role)
+    VALUES('1', 'admin@admin.ru', 'admin', 'admin', 'admin', 'admin');
 -- +goose StatementEnd
 
 -- +goose Down
